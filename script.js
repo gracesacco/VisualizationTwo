16697802ba6eb329afdd0ae45372d9280849e00d
Grace Sacco <48771641+gracesacco@users.noreply.github.com>
2020-09-14 16:24:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let attractions;
-fetch('let attractions;
-fetch('https://s3.us-west-2.amazonaws.com/secure.notion-static.com/9d7f28c1-b3cb-41cc-b1ab-c42f5b62beeb/attractions.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20200914%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20200914T153057Z&X-Amz-Expires=86400&X-Amz-Signature=ed74ce67beee91be4c32da4aff95200d577fbdd3a65066faae156bc63d4e6c71&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22attractions.json%22')
+fetch('attractions.json')
 .then(response=> response.json())
 .then(data => {
 	attractions = data;
